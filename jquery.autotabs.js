b3b7aa2c9224ab628e73529be8b85d888c081d60
Oliver Pichon <op@united-asian.com>
2010-11-29 13:41:56 +0700

    refactored js code

--- jquery.autotabs.js
+++ jquery.autotabs.js
@@ -12,40 +12,40 @@
         }, 
         options);
 
-      var ul = '<ul class="' + options['tabs_class']  + '">';
-      var active_tab = $.cookie(options['cookie_name']) || options['active_tab'];
+      var ul = '<ul class="' + options.tabs_class  + '">';
+      var active_tab = $.cookie(options.cookie_name) || options.active_tab;
 
       return $this.each(function() {
-        if ($this.children(options['tab_pane_selector']).length <= 1) { return; }
+        if ($this.children(options.tab_pane_selector).length <= 1) { return; }
         
-        $this.children(options['tab_pane_selector']).each(function(index) {
+        $this.children(options.tab_pane_selector).each(function(index) {
           ul += '<li' + (index == active_tab ? ' class="current"' : '') + '>' +
                 '<a href="#' + this.id + '">' + 
-                $(options['tab_label_selector'], $(this)).get(0).innerHTML + 
+                $(options.tab_label_selector, $(this)).get(0).innerHTML + 
                 '</a></li>'; 
         });
         ul +- '</ul>';
 
         $this.prepend(ul);
         
-        $('ul.' + options['tabs_class'] + ' li > a').click(function() {
+        $('ul.' + options.tabs_class + ' li > a').click(function() {
           var link = $(this);
-          link.parent().addClass(options['active_class']).siblings('li').removeClass(options['active_class']);
-          $(options['tab_pane_selector'] + '.' + options['active_class'], $this).slideUp('fast').removeClass(options['active_class']);
-          $this.children(options['tab_pane_selector']).each(function(index) { 
+          link.parent().addClass(options.active_class).siblings('li').removeClass(options.active_class);
+          $(options.tab_pane_selector + '.' + options.active_class, $this).slideUp('fast').removeClass(options.active_class);
+          $this.children(options.tab_pane_selector).each(function(index) { 
             if (link.attr('href') == '#' + this.id) {
-              $(this).addClass(options['active_class']).slideDown('fast)');
-              $.cookie(options['cookie_name'], index); 
+              $(this).addClass(options.active_class).slideDown('fast)');
+              $.cookie(options.cookie_name, index); 
             }
           });
           return false;
         });
 
-        $this.children(options['tab_pane_selector']).each(function(index) { 
+        $this.children(options.tab_pane_selector).each(function(index) { 
           if (index == active_tab) {
-            $(this).addClass(options['active_class']).slideDown('fast');
+            $(this).addClass(options.active_class).slideDown('fast');
           }
-          else { $(this).slideUp('fast').removeClass(options['active_class']); }
+          else { $(this).slideUp('fast').removeClass(options.active_class); }
         });      
       });
     }

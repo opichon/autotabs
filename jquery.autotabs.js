c1e686d257c0c0500a391f2d552a950ab9571691
Olivier Pichon <op@united-asian.com>
2012-11-02 08:51:07 +0700

    added vertical style

--- jquery.autotabs.js
+++ jquery.autotabs.js
@@ -3,25 +3,28 @@
     var options,
         active_tab_index,
         processing = false;
-    
+
     var methods = {
       init: function(params) {
 
-        options = $.extend({},
-          this.autotabs.defaults, 
+        options = $.extend(
+          true,
+          {},
+          this.autotabs.defaults,
           params);
-
-        var ul = '<ul class="' + options.tabs_class  + '">';
-        active_tab_index = (options.active_tab == null ? 
-                             ($.cookie ? $.cookie(options.cookie_name) : 0) : 
-                             options.active_tab) || 0;
 
         return this.each(function() {
           var $this = $(this);
+
+          var ul = '<ul class="' + options.tabs_class  + (options.vertical ? ' vertical' : '') + '">';
+          active_tab_index = (options.active_tab == null ?
+                               ($.cookie ? $.cookie(options.cookie_name) : 0) :
+                               options.active_tab) || 0;
+
           var children = $this.children(options.tab_pane_selector);
- 
+
           switch (children.length) {
-            case 0 : 
+            case 0 :
               return;
 
             case 1 :
@@ -32,6 +35,12 @@
 
             default :
               children.each(function(index, element) {
+                if (element.id == '') {
+                  var id = options.tab_pane_id && $.isFunction(options.tab_pane_id) ?
+                             options.tab_pane_id(index, element) :
+                             '___' + (index + 1);
+                  $(element).attr('id', id);
+                }
                 ul += helpers.generate_tab(index, element);
               });
           }
@@ -41,53 +50,55 @@
           var tabs = (options.tabs_selector != '') ? $(options.tabs_selector) : $this;
           if (!tabs) { tabs = $this; }
           tabs.prepend(ul);
- 
+
           $('ul.' + options.tabs_class + ' li > a', tabs).click(function() {
             if (processing) { return false; }
             processing = true;
             helpers.showLoadingIcon(options);
 
             var link = $(this);
-            
+
             link.parent().addClass(options.active_class).siblings('li').removeClass(options.active_class);
             $this.children(options.tab_pane_selector).each(function() {
               $(this).slideUp('fast').removeClass(options.active_class);
             });
-            
-            $this.children(options.tab_pane_selector).each(function(index) { 
-              if (link.attr('rel') == this.id) {
+
+            $this.children(options.tab_pane_selector).each(function(index, e) {
+              if (link.attr('rel') == $(this).attr('id')) {
                 try {
                   helpers.load(this);
                   if ($.cookie) {
-                    $.cookie(options.cookie_name, index, { path: options.cookie_path });  
+                    $.cookie(options.cookie_name, index, { path: options.cookie_path });
                   }
 
                 }
                 catch (e) { console.log(e); }
               }
-            }); 
-                
+            });
+
             return false;
           });
- 
-          $this.children(options.tab_pane_selector).each(function(index, elt) { 
+
+          $this.children(options.tab_pane_selector).each(function(index, elt) {
             if (index == active_tab_index) { helpers.load(elt); }
-            else { $(elt).slideUp('fast').removeClass(options.active_class); } 
-          }); 
-        }); 
+            else { $(elt).slideUp('fast').removeClass(options.active_class); }
+          });
+        });
       }
     };
-    
+
     var helpers = {
       generate_tab: function(index, element) {
-        var tab = '<li' + (index == active_tab_index ? ' class="' + options.active_class + '"' : '') + 
-                  (options.tab_id && $.isFunction(options.tab_id) ? ' id="' + options.tab_id(element.id) + '">' : '>') +
-                  '<a href="' + ($(element).attr('rel') || '#' + element.id) + '" rel="' + $(element).attr('id') + '">' + 
-                  ($(element).attr('title') || $(options.tab_label_selector, $(element)).get(0).innerHTML) + 
-                  '</a></li>';
-        return tab;
+        var id = options.tab_id && $.isFunction(options.tab_id) ? options.tab_id(element.id) : element.id;
+        var cls = options.tab_class + (index == active_tab_index ? ' ' + options.active_class : '');
+        var label = $(element).attr('title') ||
+                      ($(options.tab_label_selector, $(element)).length ?
+                        $(options.tab_label_selector, $(element)).get(0).innerHTML :
+                        options.tab_label && $.isFunction(options.tab_label) ? options.tab_label(index, element) : 'Tab ' + (index + 1));
+        var link = '<a href="' + ($(element).attr('rel') || '#' + element.id) + '" rel="' + element.id + '">' + label + '</a>';
+        return '<li class="' + cls + '" id="' + id + '">' + link + '</li>';
       },
-        
+
       load: function(pane) {
         var success = helpers.getSuccess(pane.id);
         if ((url = $(pane).attr('rel')) && ($.trim($(pane).html()) == '' || options.force_refresh)) {
@@ -108,21 +119,21 @@
           helpers.hideLoadingIcon(options);
         }
       },
-      
+
       getSuccess: function(id) {
         if (!options.success) { return null; }
         if ($.isFunction(options.success)) {
           return options.success;
         }
-          
+
         return options.success[id];
       },
-      
+
       showLoadingIcon: function(options) {
         if (!options.loading_icon) { return; }
         $(options.loading_icon).show();
-      }, 
-      
+      },
+
       hideLoadingIcon: function(options) {
         if (!options.loading_icon) { return; }
         $(options.loading_icon).hide();
@@ -132,19 +143,20 @@
     // Method calling logic
     if (methods[method]) {
       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-    } 
+    }
     else if (typeof method === 'object' || !method) {
       return methods.init.apply(this, arguments);
-    } 
+    }
     else {
       $.error('Method ' +  method + ' does not exist in jQuery.autotabs');
     }
   };
-  
+
   $.fn.autotabs.defaults = {
-      tab_pane_selector: "div",
-      tab_label_selector: "h3",
+      tab_pane_selector: "div, section, .tab-pane",
+      tab_label_selector: "h3, :first-child",
       tabs_class: "autotabs",
+      tab_class: "autotab",
       tabs_selector: "",
       active_class: "current",
       active_tab: null,
@@ -153,6 +165,9 @@
       force_refresh: false,
       tab_orphans: false,          // display a tab even for a single orphan child element
       loading_icon: '#loading',
-      tab_id: function(id) { return '__' + id; }
+      tab_id: function(id) { return '__' + id; },
+      tab_label: function(index, element) { return 'Tab ' + (index + 1); },
+      tab_pane_id: function(index, element) { return '___' + (index + 1); },
+      vertical: false
   };
 })(jQuery);
